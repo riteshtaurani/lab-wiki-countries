e47b8dc7a14e537358bfe2c29b580716fd8ff97f
Ritesh <tauraniritesh@gmail.com>
2020-07-17 11:21:53 +0200

    partial

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data.json'
+import { Link } from 'react-router-dom'
+
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <nav className="navbar navbar-dark bg-primary mb-3">
+        <div className="container">
+          <a className="navbar-brand" href="/">WikiCountries</a>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="row">
+        <div className="col-5" style={{ maxHeight: '90vh', overflow: 'scroll'}}>
+            <div className="list-group">
+            {data.map(c => {
+                return (
+                  <div className="list-group-item list-group-item-action" key={c.cca3}>
+                    <Link to={`/${c.cca3}`}>{c.name.common} active</Link>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+          
+        </div>
+      </div>
     </div>
+    </div>
+    
   );
 }
 
